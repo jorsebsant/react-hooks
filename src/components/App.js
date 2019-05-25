d646d5a241a462fd2c012100f2a726bc8e551371
Jorge Santamaria <jorsebsant@gmail.com>
2019-05-25 04:04:21 -0500

    add axios and make the call to the numbers public API

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,26 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import Form from './Form'
 import '../styles/App.css';
 
 function App() {
+  
+  const [welcomeMessage, setWelcomeMessage] = useState("Welcome to Numbers Trivia!")
 
   // this hook acts like componentdidmount and didupdate at the same time, if we only want it to be executed one time
   // the second parameter should be an empty array, if we want it to update multiple times, the array should contain variables
   // and when those variables updates the hook will trigger as the didupdate lyfecicle
   useEffect(()=>{
     setTimeout(()=>{
-      const welcomeMessage = document.getElementById("welcomeMessage")
-      welcomeMessage.innerHTML =  "Try Out Our Trivia Generator!"
+      setWelcomeMessage("Try Out Our Trivia Generator!")
     }, 3000)
   }, [])
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1 id="welcomeMessage">Welcome to Numbers Trivia!</h1>
+        <h1 id="welcomeMessage">
+          {welcomeMessage}
+        </h1>
         <Form/>
       </header>
     </div>
