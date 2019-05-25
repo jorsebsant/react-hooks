2cfc209b621e3338a5443a48f2181b42e7cb8bb8
jorsebsant <jorsebsant@gmail.com>
2019-05-24 22:06:05 -0500

    Revert "add first hook with a settimeout"
    This reverts commit 02b6d6a0ef3b63e968e83b875ff53898cbef643c.

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,11 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import '../styles/App.css';
 
 function App() {
-
-  // this hook acts like componentdidmount and didupdate at the same time, if we only want it to be executed one time
-  // the second parameter should be an empty array, if we want it to update multiple times, the array should contain variables
-  // and when those variables updates the hook will trigger as the didupdate lyfecicle
-  useEffect(()=>{
-    setTimeout(()=>{
-      const welcomeMessage = document.getElementById("welcomeMessage")
-      welcomeMessage.innerHTML =  "Try Out Our Trivia Generator!"
-    }, 3000)
-  }, [])
-
   return (
     <div className="App">
       <header className="App-header">
-        <h1 id="welcomeMessage">Welcome to Numbers Trivia!</h1>
+        <h1>Hello, World!</h1>
       </header>
     </div>
   );
